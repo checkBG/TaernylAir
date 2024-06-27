cdaafe1f04d568a92341c1ba35f326b0264da52f
Alexander Chernyshov <alcherng2015@gmail.com>
2024-06-27 16:53:58 +0300

    Fix

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,9 +1,8 @@
-@file:Suppress("DEPRECATION")
-
 import java.net.URL
 
 fun main() {
-    
+    val flight = fetchFlight()
+    println(flight)
 }
 
 fun fetchFlight(url: String): String = URL(url).readText()
