7b0abbb563c5b1765ff2c124c54e13aa4f9bb142
Alexander Chernyshov <alcherng2015@gmail.com>
2024-06-27 18:22:49 +0300

    Add new urls

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,8 +1,11 @@
 import java.net.URL
 
+const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+
 fun main() {
     val flight = fetchFlight()
     println(flight)
 }
 
-fun fetchFlight(url: String): String = URL(url).readText()
+fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
