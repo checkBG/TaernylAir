b9a32b946e10d8b6a36780b9203908fbe23c7925
Alexander Chernyshov <alcherng2015@gmail.com>
2024-07-01 21:34:22 +0300

    Add data class to parse

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,28 +1,33 @@
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import java.net.URL
-import kotlinx.coroutines.runBlocking
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
-const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
-const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
 
 fun main() {
     runBlocking {
         println("Started")
         launch {
-            val flight = fetchFlight()
+            val flight = fetchFlight("Violett")
             println(flight)
         }
         println("Finished")
     }
 }
 
-suspend fun fetchFlight(): String {
+suspend fun fetchFlight(passengerName: String): FlightStatus {
     val client = HttpClient(CIO)
-    return client.get(FLIGHT_ENDPOINT, ).bodyAsText()
+    val flightResponse = client.get(FLIGHT_ENDPOINT).bodyAsText()
+    val loyaltyResponse = client.get(LOYALTY_ENDPOINT).bodyAsText()
+
+    return FlightStatus.parse(
+        flightResponse = flightResponse,
+        loyaltyResponse = loyaltyResponse,
+        passengerName = passengerName
+    )
 }
