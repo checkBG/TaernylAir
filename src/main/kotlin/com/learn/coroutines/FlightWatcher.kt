e4d4bb16d2ae49b3c25ffbbf58483857275d62a8
Alexander Chernyshov <alcherng2015@gmail.com>
2024-07-10 23:20:41 +0300

    Added new val flightsAtGate to follow how count of flights is there and wrapped it  in the launch

--- src/main/kotlin/com/learn/coroutines/FlightWatcher.kt
+++ src/main/kotlin/com/learn/coroutines/FlightWatcher.kt
@@ -3,38 +3,55 @@ package com.learn.coroutines
 import com.learn.coroutines.BoardingState.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
         println("Getting the latest flight info...")
+
         val flights = fetchFlights()
         val flightDescriptions = flights.joinToString {
             "${it.passengerName} (${it.flightNumber})"
         }
+
         println("Found flights for $flightDescriptions")
-        flights.forEach {
-            watchFlight(it)
+
+        val flightsAtGate = MutableStateFlow(value = flights.size)
+
+        launch {
+            flightsAtGate.collect { flightCount ->
+                println("There are $flightCount flights being tracked")
+            }
+        }
+
+        launch {
+            flights.forEach {
+                watchFlight(it)
+//                flightsAtGate.value--
+            }
         }
     }
 }
 
 suspend fun watchFlight(initialFlight: FlightStatus) {
     val passengerName = initialFlight.passengerName
+
     val currentFlight: Flow<FlightStatus> = flow {
         var flight = initialFlight
+
         while (flight.departureTimeInMinutes >= 0 && !flight.isFlightCanceled) {
             emit(flight)
-            delay(1000)
-            emit(flight)
+            delay(100)
             flight = flight.copy(
                 departureTimeInMinutes = flight.departureTimeInMinutes - 1
             )
         }
     }
 
-    currentFlight.collect{
+    currentFlight.collect {
         val status = when (it.boardingStatus) {
             FlightCanceled -> "Your flight was canceled"
             BoardingNotStarted -> "Boarding will start soon"
